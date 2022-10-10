130bd58368099c2266ec85deb8e03f61408d111d
yukaiquan <1962568272@qq.com>
2022-10-10 12:07:07 +0800

    uniprot query api get debug

--- uniport/api/get_uniport.py
+++ uniport/api/get_uniport.py
@@ -1,34 +1,31 @@
+#!/use/bin/env python3
 '''
 Copyright [yukaiquan 1962568272@qq.com]
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
     http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-
 input_file = `
 Penicillin binding protein 3
 Histidine decarboxylase
 Nitric oxide synthase, inducible
 `
 output_file = `
-Penicillin binding protein 3 ['P33317', 'A0A2T2NDB6', 'A0A2N0V3U4', 'A0A8I2JQW5',...]
+Penicillin binding protein 3 'P33317', 'A0A2T2NDB6', 'A0A2N0V3U4', 'A0A8I2JQW5'
 Histidine decarboxylase ['P0A9K8', 'A0A2T2NDB6', 'A0A2N0V3U4', 'A0A8I2JQW5',...]
 Nitric oxide synthase, inducible ['P0A9K8', 'A0A2T2NDB6', 'A0A2N0V3U4', 'A0A8I2JQW5',...]
-
 `
 '''
 
 import requests
 from urllib import parse
 import sys
+import time
 
 
 CONFIG = {
@@ -36,13 +33,15 @@ CONFIG = {
     "accept": "application/json",
 }
 
-# url = r"https://rest.uniprot.org/uniprotkb/search?query=%20Vasopressin%20V1a%20receptor"
+# url = r"https://rest.uniprot.org/uniprotkb/search?size=1&query=Vasopressin%20V1a%20receptor"
+# https://rest.uniprot.org/uniprotkb/search?size=5&query=Penicillin%20binding%20protein%203
 
 
 def get_json_uniprot_id(url_list: list, CONFIG: dict) -> dict:
     uniport_dict: dict = {}
     uniport_list: list = []
     for url in url_list:
+        # print(url)
         res_htlm = requests.get(url, headers=CONFIG, timeout=10)
         # 逆向解析
         name = parse.unquote(url.split('query=')[1])
@@ -51,15 +50,20 @@ def get_json_uniprot_id(url_list: list, CONFIG: dict) -> dict:
                 json_data = res_htlm.json()
 
                 for uniport_id in json_data['results']:
-                    uniport_list.append(uniport_id['primaryAccession'])
+                    # just one best match to dict
+                    if name not in uniport_dict:
+                        uniport_dict[name] = uniport_id['primaryAccession']
+                    else:
+                        break
+                    # uniport_list.append(uniport_id['primaryAccession'])
             except:
                 print(name, "not found in uniprot 404")
 
         else:
             print(
                 f"Failed to get Uniport ID{name}, status code: {res_htlm.status_code}")
-        uniport_dict[name] = uniport_list
-        # time.sleep(0.01)
+        # uniport_dict[name] = uniport_list
+        time.sleep(0.01)
     return uniport_dict
 
 
@@ -69,11 +73,12 @@ def generator_url(uniportname_list: list) -> list:
         # 正向解析
         name_filds = parse.quote(uniportname)
         url_list.append(
-            f"https://rest.uniprot.org/uniprotkb/search?query=%20{name_filds}")
+            f"https://rest.uniprot.org/uniprotkb/search?size=2&query={name_filds}")
     return url_list
 
 
 def main(input_id: str, output_file: str):
+    start_time = time.time()
     uniportname_list: list = []
     with open(input_id, 'r') as f:
         for line in f.readlines():
@@ -83,6 +88,8 @@ def main(input_id: str, output_file: str):
     with open(output_file, 'w') as f:
         for key, value in uniport_dict.items():
             f.write(f"{key}\t{value}\n")
+    stop_time = time.time()
+    print(f"Total time: {stop_time-start_time}")
 
 
 if __name__ == "__main__":
